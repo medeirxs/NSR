a63db3be34e3607d184a2d6ef3f84e14be14e1e7
Matheus Medeiros <tteurj@icloud.com>
2025-05-07 04:25:36 -0300

    refactor: make main routes and hide status bar and system navigation

--- main.lua
+++ main.lua
@@ -1,7 +1,29 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+local composer = require("composer")
+local userData = require("lib.userData")
 
--- Your code here
+display.setStatusBar(display.HiddenStatusBar)
+
+native.setProperty("androidSystemUiVisibility", "immersiveSticky")
+if (system.getInfo("platformName") == "Android") then
+    local androidVersion = string.sub(system.getInfo("platformVersion"), 1, 3)
+    if (androidVersion and tonumber(androidVersion) >= 4.4) then
+        native.setProperty("androidSystemUiVisibility", "immersiveSticky")
+    elseif (androidVersion) then
+        native.setProperty("androidSystemUiVisibility", "lowProfile")
+    end
+end
+
+local data = userData.load()
+
+if data and data.id and data.server then
+    -- composer.gotoScene("router.home", {
+    composer.gotoScene("router.home", {
+        --     effect = "fade",
+        time = 1
+    })
+else
+    composer.gotoScene("router.auth", {
+        effect = "fade",
+        time = 1
+    })
+end
