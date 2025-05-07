e962c4978c266910d816ec1e3ee1ad9dead5249e
Matheus Medeiros <tteurj@icloud.com>
2025-05-07 04:31:30 -0300

    docs: create and config supabase connection to db

---
+++ config/supabase.lua
@@ -0,0 +1,7 @@
+local supabase = {}
+
+supabase.SUPABASE_URL = "https://majjmbdhtgifpqwluigz.supabase.co"
+supabase.SUPABASE_ANON_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hamptYmRodGdpZnBxd2x1aWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0ODU3MjEsImV4cCI6MjA2MDA2MTcyMX0.AP52k8bptv1xUWWJ1bwTJpOPeavskrPyV5rkIZWocn8"
+
+return supabase
